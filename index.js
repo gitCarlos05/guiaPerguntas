d42e3df1a7165cef88ca01aba96b9e0f26a80b7c
Carlos Valdir <valdircarlos2005@gmail.com>
2023-07-12 23:26:52 -0300

    inicial commit

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+var exibirMensagem = true;
+var produtos = [
+    { nome: "Doritos", preco: 7.58 },
+    { nome: "Pipoca", preco: 5.47 },
+    { nome: "Passatempo", preco: 3.56 },
+    { nome: "Fini mini", preco: 1.47 },
+    { nome: "Coca-cola 2L", preco: 8.99 },
+    { nome: "Energetico 2L", preco: 8.80 },
+];
 
 app.set('view engine', 'ejs');
 
@@ -7,21 +16,25 @@ app.get("/", (requisicao, resposta) => {
     resposta.render("../views/index", {
         name: "Parâmetro nome aqui",
         language: "Parâmetro language aqui",
-        empresa: "Udemy",
-        idade: "17 anos"
+        empresa: "Parâmetro empresa aqui",
+        idade: "Parâmetro idade aqui",
+        mensagem: exibirMensagem,
+        produtos: produtos
     })
 });
 
-app.get("/:nome/:language", (requisicao, resposta) => {
+app.get("/:nome/:language/:empresa/:idade", (requisicao, resposta) => {
     var nome = requisicao.params.nome;
     var favoriteLanguage = requisicao.params.language;
-    var exibirMensagem = true;
+    var empresa = requisicao.params.empresa;
+    var idade = requisicao.params.idade;
     resposta.render("../views/index", {
         name: nome,
         language: favoriteLanguage,
-        empresa: "Udemy",
-        idade: "17 anos",
-        mensagem: exibirMensagem
+        empresa: empresa,
+        idade: idade,
+        mensagem: exibirMensagem,
+        produtos: produtos
     });
 });
 
